e8ec0f4eda83a28990360bd734f9062b92184da3
Chatbordin <klinscea@gmail.com>
2025-02-10 09:19:50 +0700

    fix: redirect to '/substation' and comment out authentication logic

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react'
 import Typography from '@mui/material/Typography'
-import { auth } from '../../auth'
+// import { auth } from '../../auth'
+import { redirect } from 'next/navigation'
 
 export default async function HomePage() {
-  const session = await auth()
+  // const session = await auth()
+
+  void redirect('/substation')
 
   return (
     <Typography>
-      Welcome to Toolpad, {session?.user?.name || 'User'}!
+      {/* Welcome to Toolpad, {session?.user?.name || 'User'}! */}
     </Typography>
   )
 }
