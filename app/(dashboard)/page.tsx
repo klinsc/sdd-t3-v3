6af3c07375b17b140358d9fc72ff23d140ec276b
klinsc <klinscea@gmail.com>
2025-02-12 21:31:13 +0700

    feat: redirect to substation page and comment out session handling

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,12 +1,15 @@
-import Typography from '@mui/material/Typography'
-import { auth } from '../../auth'
+// import Typography from '@mui/material/Typography'
+import { redirect } from 'next/navigation'
 
 export default async function HomePage() {
-  const session = await auth()
+  // const session = await auth()
 
-  return (
-    <Typography>
-      Welcome to Toolpad, {session?.user?.name || 'User'}!
-    </Typography>
-  )
+  // redirect to substation page
+  redirect('/substation')
+
+  // return (
+  //   <Typography>
+  //     Welcome to Toolpad, {session?.user?.name || 'User'}!
+  //   </Typography>
+  // )
 }
