628467f3aa5565d46fbc1f2c2b29218b9df72f8d
klinsc <klinscea@gmail.com>
2025-07-06 17:40:23 +0700

    added: chat

--- app/(dashboard)/orders/page.tsx
+++ app/(dashboard)/orders/page.tsx
@@ -1,6 +1,21 @@
-import * as React from 'react'
+import SubstationChat from '@/app/_components/SubstationChat'
 import Typography from '@mui/material/Typography'
 
 export default async function OrdersPage() {
-  return <Typography>Welcome to the Toolpad orders!</Typography>
+  return (
+    <>
+      <Typography
+        variant="h4"
+        sx={{ mb: -0.5, fontWeight: 'bold', color: 'primary.main' }}>
+        น้องกอฟ (ทดลองใช้)
+      </Typography>
+
+      <SubstationChat />
+      <Typography variant="body2" color="text.secondary">
+        <strong>คำเตือน:</strong> น้องกอฟอาจจะตอบผิดพลาดได้
+        กรุณาตรวจสอบข้อมูลก่อนนำไปใช้
+        <br />
+      </Typography>
+    </>
+  )
 }
