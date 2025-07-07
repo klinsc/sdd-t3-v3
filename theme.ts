5e602b70c68fa45ef1ecef76e19f347c13e7da82
Chatbordin <klinscea@gmail.com>
2025-07-07 15:24:48 +0700

    disabled: dark mode

--- theme.ts
+++ theme.ts
@@ -6,9 +6,17 @@ const theme = createTheme({
   cssVariables: {
     colorSchemeSelector: 'data-toolpad-color-scheme',
   },
-  colorSchemes: { light: true, dark: true },
+  colorSchemes: { light: true, dark: false },
   typography: {
     fontFamily: 'Noto Sans Thai, Roboto, sans-serif',
+  },
+  palette: {
+    primary: {
+      main: '#74055f',
+    },
+    secondary: {
+      main: '#c99427',
+    },
   },
 })
 
